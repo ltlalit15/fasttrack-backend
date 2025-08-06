03d222443f52ebe281f0006c41252be6f04ca7e4
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-06 17:44:42 +0530

    Update feedbackRoute.js

--- Routers/feedbackRoute.js
+++ Routers/feedbackRoute.js
@@ -1,9 +1,9 @@
 
 import express from "express";
-import { submitFeedback, getFeedback } from "../Controllers/feedback.js";
+import { submitFeedback, getFeedback , deletefeedback} from "../Controllers/feedback.js";
 
 const router = express.Router();
 router.post("/feedback", submitFeedback);
 router.get("/feedback", getFeedback);
-
+router.delete("/deletefeedback/:id", deletefeedback);
 export default router;
