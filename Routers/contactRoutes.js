42dd5e0be6df686922a297fc7230c3ea076b46b0
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-06 17:44:02 +0530

    Update contactRoutes.js

--- Routers/contactRoutes.js
+++ Routers/contactRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { submitContactForm, getContactForm } from '../Controllers/contactController.js';
+import { submitContactForm, getContactForm ,deleteContactForm } from '../Controllers/contactController.js';
 
 const router = express.Router();
 
 router.post('/contact', submitContactForm);
 router.get('/contact', getContactForm);
+router.delete('/deleteContactForm/:id', deleteContactForm);
 
 
 export default router;
