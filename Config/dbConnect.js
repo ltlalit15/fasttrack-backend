4c5d2a320284139fafa6854479a3cde6ede5ec76
GautamBairagi <gauatambairagi22@gmail.com>
2025-08-07 18:10:12 +0530

    s

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -1,38 +1,13 @@
 
-// import { JSONCookie } from 'cookie-parser';
-// import { query } from 'express';
-// import mysql from 'mysql2/promise';
-// export const pool = mysql.createPool({
-//   host: "switchyard.proxy.rlwy.net",   // ✅ Railway host
-//   port: 16378,                          // ✅ Railway port
-//   user: "root",                         // ✅ Railway username
-//   password: "JiIWGiXmyXtxNUKotWvRqmKCeIkSMTPS", // ✅ Railway password
-//   database: "railway",                 // ✅ Railway DB name
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000,
-// });
-// (async () => {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log(" Local MySQL database connected !!" );
-//     connection.release();
-//   } catch (error) {
-//     console.error("❌ Database connection error:", error);
-//   }
-// })();
-
-
 import { JSONCookie } from 'cookie-parser';
 import { query } from 'express';
 import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
-  host: "localhost",     // 👈 Localhost for local MySQL
-  port: 3306,            // 👈 Default MySQL port
-  user: "root",          // 👈 Your local MySQL username
-  password: "",          // 👈 Or your local MySQL password
-  database: "fasttrack",                // Database Name
+  host: "switchyard.proxy.rlwy.net",   // ✅ Railway host
+  port: 16378,                          // ✅ Railway port
+  user: "root",                         // ✅ Railway username
+  password: "JiIWGiXmyXtxNUKotWvRqmKCeIkSMTPS", // ✅ Railway password
+  database: "railway",                 // ✅ Railway DB name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -47,3 +22,28 @@ export const pool = mysql.createPool({
     console.error("❌ Database connection error:", error);
   }
 })();
+
+
+// import { JSONCookie } from 'cookie-parser';
+// import { query } from 'express';
+// import mysql from 'mysql2/promise';
+// export const pool = mysql.createPool({
+//   host: "localhost",     // 👈 Localhost for local MySQL
+//   port: 3306,            // 👈 Default MySQL port
+//   user: "root",          // 👈 Your local MySQL username
+//   password: "",          // 👈 Or your local MySQL password
+//   database: "fasttrack",                // Database Name
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 10000,
+// });
+// (async () => {
+//   try {
+//     const connection = await pool.getConnection();
+//     console.log(" Local MySQL database connected !!" );
+//     connection.release();
+//   } catch (error) {
+//     console.error("❌ Database connection error:", error);
+//   }
+// })();
